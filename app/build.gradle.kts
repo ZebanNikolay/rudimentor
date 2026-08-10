2af70aac2e50747b6b272970426382cbd59abdcf
ZebanNikolay <zebannikolay@gmail.com>
2026-08-10 11:19:14 +0000

    feat(metronome): toolbar with back key and wordmark, haptic on hand swap, balanced row pitch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ val developmentKeyAlias = providers.environmentVariable("RUDIMENTOR_DEBUG_KEY_AL
     .orElse("androiddebugkey")
 val developmentKeyPassword = providers.environmentVariable("RUDIMENTOR_DEBUG_KEY_PASSWORD")
     .orElse(developmentKeystorePassword)
-val appVersionName = "0.1.0-dev.9"
-val appVersionCode = 9
+val appVersionName = "0.1.0-dev.10"
+val appVersionCode = 10
 
 base {
     archivesName.set("RudiMentor-$appVersionName-build-$appVersionCode")
