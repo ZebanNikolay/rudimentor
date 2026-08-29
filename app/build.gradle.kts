a0d25d38c4bac7c1b34446490f16ae004d02bad1
ZebanNikolay <zebannikolay@gmail.com>
2026-08-30 01:12:15 +0200

    Bump to 0.1.0-dev.46 (build 46)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ val developmentKeyPassword = providers.environmentVariable("RUDIMENTOR_DEBUG_KEY
 val allowLocalDebugSigning = providers.gradleProperty("rudimentor.localDebugSigning")
     .map(String::toBoolean)
     .orElse(false)
-val appVersionName = "0.1.0-dev.45"
-val appVersionCode = 45
+val appVersionName = "0.1.0-dev.46"
+val appVersionCode = 46
 
 base {
     archivesName.set("RudiMentor-$appVersionName-build-$appVersionCode")
