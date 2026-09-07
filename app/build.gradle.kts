30682a857d492434b48c7e21bacb612590caa350
ZebanNikolay <zebannikolay@gmail.com>
2026-09-07 22:11:11 +0200

    Version 1.0.0-dev.58

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ val uploadKeyPassword = providers.environmentVariable("RUDIMENTOR_UPLOAD_KEY_PAS
 //   1.0.0-dev.N  sandbox build installed on the developer's phone
 //   1.0.0-rc.N   candidate uploaded to the internal / closed testing track
 //   1.0.0        production
-val appVersionName = "1.0.0-dev.57"
-val appVersionCode = 57
+val appVersionName = "1.0.0-dev.58"
+val appVersionCode = 58
 
 base {
     archivesName.set("RudiMentor-$appVersionName-build-$appVersionCode")
